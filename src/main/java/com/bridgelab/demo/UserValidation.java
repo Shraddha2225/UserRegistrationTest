576bbaa66cecb8da402ba0e21b8fe0c4b450f94c
Shraddha2225 <shraddhat22m@gmail.com>
2020-02-19 19:22:41 +0530

    ADDED:Check First And Last Name Start With Capital Letter

--- src/main/java/com/bridgelab/demo/UserValidation.java
+++ src/main/java/com/bridgelab/demo/UserValidation.java
@@ -4,4 +4,11 @@ public class UserValidation {
     public static void main(String[] args) {
         System.out.println("WELCOME TO USER REGISTRATION");
     }
+    public static String validateFirstAndLastName(String fname) {
+        String firstNamePattern = "^[A-Z]{1}[a-zA-Z]{3,}$";
+        if (fname.matches(firstNamePattern)) {
+            return "Valid";
+        }
+        return "Invalid";
+    }
 }
---
+++ src/test/java/com/bridgelab/demo/UserRegistrationTest.java
@@ -0,0 +1,31 @@
+package com.bridgelab.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnValid() {
+        UserValidation userValidation = new UserValidation();
+        String result=userValidation.validateFirstAndLastName("Shraddha");
+        Assert.assertEquals("Valid",result);
+    }
+    @Test
+    public void givenFirstName_WhenInproper_ShouldReturnInvalid() {
+        UserValidation userValidation = new UserValidation();
+        String result=userValidation.validateFirstAndLastName("shraddha");
+        Assert.assertEquals("Invalid",result);
+    }
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnValid() {
+        UserValidation userValidation = new UserValidation();
+        String result=userValidation.validateFirstAndLastName("Meshram");
+        Assert.assertEquals("Valid",result);
+    }
+    @Test
+    public void givenLastName_WhenInproper_ShouldReturnInvalid() {
+        UserValidation userValidation = new UserValidation();
+        String result=userValidation.validateFirstAndLastName("meshram");
+        Assert.assertEquals("Invalid",result);
+    }
+}
